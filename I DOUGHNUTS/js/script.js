f01cf2417cbfba50444dac9171622333f64a6bf9
ito011829 <jarvismark8_29@outlook.jp>
2025-03-30 10:04:42 +0900

    I DOUGHNUTS upload

---
+++ I DOUGHNUTS/js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+  // carousel
+  $('.carousel').slick({
+    autoplay: true,
+    dots: false,
+    infinite: true,
+    autoplayspeed: 3000,
+    arrows: true,
+    fade: true,
+    speed: 1500,
+  });
+});
